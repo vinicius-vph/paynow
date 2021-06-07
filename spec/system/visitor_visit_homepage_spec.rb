f73c712c9a6cc3b78da9640767bf8da1452e49bc
Vinicius Santos <vinicius.vph@gmail.com>
2021-06-07 17:52:34 -0300

    created home page visit test

---
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Visitor visit homepage' do
+   it '- Should be able to access homepage' do
+    visit root_path
+
+    expect(page).to have_css('h1', text: 'Bem vindo ao PayNow')
+    expect(page).to have_css('p', text: 'Receba pagamentos, e resolva suas finanças em um só lugar')
+    expect(page).to have_css('p', text: 'Venda com mais meios de pagamento e tenha seu dinheiro na hora')
+    expect(page).to have_text('Entrar')
+    expect(page).to have_text('Crie sua conta')
+    expect(page).to have_text('Área do admin')
+  end
+end
