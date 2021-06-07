5522c505c5b3d040d14ff35f8209d6a52c166776
Vinicius Santos <vinicius.vph@gmail.com>
2021-06-07 17:50:42 -0300

    add bootstrap and favicon

--- spec/support/login_default.rb
+++ spec/support/login_default.rb
@@ -1,10 +1,10 @@
 module LoginDefault
-    def admin_one_login(admin_one = Admin.create!(name: 'admin_one@paynow.com', password: '123mudar'))
+    def admin_one_login(admin_one = Admin.create!(email: 'admin_one@paynow.com', password: '123mudar'))
         login as admin_one, scope: :admin
         admin_one
     end
 
-    def admin_two_login(admin_two = Admin.create!(name: 'admin_two@paynow.com', password: '123mudar'))
+    def admin_two_login(admin_two = Admin.create!(email: 'admin_two@paynow.com', password: '123mudar'))
         login as admin_two, scope: :admin
         admin_two
     end
