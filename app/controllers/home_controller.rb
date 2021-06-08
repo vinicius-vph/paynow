5674fbbd0007bf3c7514f28c2b69855235a5cd98
Vinicius Santos <vinicius.vph@gmail.com>
2021-06-08 17:00:58 -0300

    css and application layout adjusts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
+    layout :verify_layout
+
     def index
+    end
+  
+    private
+  
+    def verify_layout
+      if admin_signed_in?
+        'admin'
+      else
+        'application'
+      end
     end
 end
