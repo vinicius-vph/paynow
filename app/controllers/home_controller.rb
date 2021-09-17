3b075625a11d8ecdcdc020777473367d3d0717d6
Vinicius Santos <vinicius.vph@gmail.com>
2021-09-17 16:28:25 -0300

    html optimizations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,58 +1,56 @@
 class HomeController < ApplicationController
-    before_action :is_signed_in?
+  before_action :is_signed_in?
 
-    def index
+  def index; end
+
+  def register; end
+
+  def new_register_companies
+    @company = Company.new
+  end
+
+  def create_register_companies
+    @company = Company.create!(company_params)
+
+    if @company
+      redirect_to register_clients_path(id: @company,
+                                        name: @company.fantasy_name,
+                                        pattern: @company.company_admin_email.split('@').last),
+                  layout: 'application', notice: t('.success')
+    else
+      @company = Company.new
+      render 'register/companies', layout: 'application', alert: t('.fail')
     end
+  end
 
-    def register
+  def new_register_clients
+    @client = Client.new
+  end
+
+  def create_register_clients
+    @client = Client.create!(client_params)
+
+    if @client
+      redirect_to new_client_session_path, layout: 'application', notice: t('.success')
+    else
+      @client = Client.new
+      render 'register/clients', layout: 'application', alert: t('.fail')
     end
+  end
 
-    def new_register_companies
-        @company = Company.new
-    end
+  private
 
-    def create_register_companies
-        @company = Company.create!(company_params)
+  def company_params
+    params.require(:company).permit(:fantasy_name, :cnpj, :corporate_name, :billing_address, :billing_email,
+                                    :company_admin_email)
+  end
 
-        if @company
-            redirect_to register_clients_path(id: @company, 
-                                                name: @company.fantasy_name, 
-                                                pattern: @company.company_admin_email.split('@').last), 
-                                                layout: "application", notice: t('.success')
-        else
-            @company = Company.new
-            render "register/companies", layout: "application", alert: t('.fail')
-        end
-    end
+  def client_params
+    params.require(:client).permit(:email, :password, :company_id)
+  end
 
-    def new_register_clients
-        @client = Client.new
-    end
-
-    def create_register_clients
-        @client = Client.create!(client_params)
-
-        if @client
-            redirect_to new_client_session_path, layout: "application", notice: t('.success')
-        else
-            @client = Client.new
-            render "register/clients", layout: "application", alert: t('.fail')
-        end 
-    end
-
-
-    private
-
-    def company_params
-        params.require(:company).permit(:fantasy_name, :cnpj, :corporate_name, :billing_address, :billing_email, :company_admin_email)
-    end
-
-    def client_params
-        params.require(:client).permit(:email, :password, :company_id)
-    end
-
-    def is_signed_in?
-        redirect_to "/admin", layout: "application" if admin_signed_in?
-        redirect_to "/client", layout: "application" if client_signed_in?
-    end
+  def is_signed_in?
+    redirect_to '/admin', layout: 'application' if admin_signed_in?
+    redirect_to '/client', layout: 'application' if client_signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
+
   devise_for :admins
   devise_for :clients
-  
+
   namespace :admin do
     root to: 'admin#index'
     resources :payment_methods
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :client do
     root to: 'client#index'
     resources :payment_methods, only: %i[index] do
-      resources :pix_client_pms, only: %i[new create] 
+      resources :pix_client_pms, only: %i[new create]
       resources :boleto_bancario_client_pms, only: %i[new create]
       resources :cartao_de_credito_client_pms, only: %i[new create]
     end
@@ -24,9 +24,9 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'register', to: 'home#register' 
-  get 'register/companies', to: 'home#new_register_companies' 
-  post 'register/companies', to: 'home#create_register_companies' 
-  get 'register/clients', to: 'home#new_register_clients' 
-  post 'register/clients', to: 'home#create_register_clients' 
+  get 'register', to: 'home#register'
+  get 'register/companies', to: 'home#new_register_companies'
+  post 'register/companies', to: 'home#create_register_companies'
+  get 'register/clients', to: 'home#new_register_clients'
+  post 'register/clients', to: 'home#create_register_clients'
 end
